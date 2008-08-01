b365237c417227761bf503b3284ed664ca40765b
Hampton Catlin <hcatlin@greed.local>
2008-08-01 13:57:22 -0400

    collecting better results

--- lib/rsp.rb
+++ lib/rsp.rb
@@ -13,10 +13,26 @@ module RSP
   end
   
   def self.run
+    game_record = {}
+    @@bots.each do |bot|
+      game_record[bot] = {:wins => 0, :loses => 0, :draws => 0}
+    end
+    
     @@bots.each do |bot|
       (@@bots - [bot]).each do |other_bot|
         game = Game.new(bot, other_bot)
         game.execute
+        
+        if game.winner == bot
+          game_record[bot][:wins]        += 1
+          game_record[other_bot][:loses] += 1
+        elsif game.winner == other_bot
+          game_record[other_bot][:wins]  += 1
+          game_record[bot][:loses]       += 1
+        else
+          game_record[other_bot][:draws] += 1
+          game_record[bot][:draws]       += 1
+        end
       end
     end
   end
