efb25409db9db742d2e5f02df0ebd5a7589ca3fd
mike <mike@mikeulas.(none)>
2008-08-01 12:09:07 -0400

    cleanup; run.rb is now executable

--- run.rb
+++ run.rb
@@ -1,11 +1,12 @@
+#!/usr/bin/env ruby
 require 'lib/rsp'
 require 'lib/bot'
 require 'lib/move'
 require 'lib/round'
 require 'lib/game'
 
-Dir.entries("bots").select { |a| a =~ /.*rb/ }.each do |file|
-  require "bots/#{file[0..-4]}"
+Dir.glob("bots/*.rb").each do |file|
+  require file
 end
 
 
